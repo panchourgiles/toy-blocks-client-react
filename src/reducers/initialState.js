e0c3bfac39730391e93d9ae5657846b32c60e18b
Pancho Urgiles <panchourgiles@gmail.com>
2021-01-19 20:05:24 -0500

    Add blocks inside nodes in initialState

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -5,25 +5,45 @@ export default () => ({
         url: 'https://thawing-springs-53971.herokuapp.com',
         online: false,
         name: 'Node 1',
-        loading: false
+        loading: false,
+        blocks: {
+          data: [],
+          loading: false,
+          error: false
+        }
       },
       {
         url: 'https://secret-lowlands-62331.herokuapp.com',
         online: false,
         name: 'Node 2',
-        loading: false
+        loading: false,
+        blocks: {
+          data: [],
+          loading: false,
+          error: false
+        }
       },
       {
         url: 'https://calm-anchorage-82141.herokuapp.com',
         online: false,
         name: 'Node 3',
-        loading: false
+        loading: false,
+        blocks: {
+          data: [],
+          loading: false,
+          error: false
+        }
       },
       {
         url: 'http://localhost:3002',
         online: false,
         name: 'Node 4',
-        loading: false
+        loading: false,
+        blocks: {
+          data: [],
+          loading: false,
+          error: false
+        }
       }
     ]
   }

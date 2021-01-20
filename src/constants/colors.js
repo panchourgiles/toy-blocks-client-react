0159e5bd60d70d8d6070e350577f8ecb065dd263
Pancho Urgiles <panchourgiles@gmail.com>
2021-01-19 20:03:17 -0500

    Add some colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,11 +1,13 @@
 export default {
-  text: "#263238",
-  faded: "#8a9094",
-  primary: "#5bb7db",
-  danger: "#D94238",
-  warning: "#f4c20d",
-  success: "#1E9A57",
-  contentBackground: "#f8f8f8",
-  border: "#aaaaaa",
-  white: "#ffffff",
+  text: '#263238',
+  faded: '#8a9094',
+  primary: '#5bb7db',
+  danger: '#D94238',
+  warning: '#f4c20d',
+  success: '#1E9A57',
+  contentBackground: '#f8f8f8',
+  border: '#aaaaaa',
+  white: '#ffffff',
+  blockBackground: '#e0e0e0',
+  title: '#3f5bfb'
 };
